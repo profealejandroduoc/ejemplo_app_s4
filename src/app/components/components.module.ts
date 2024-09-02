5a27c29c263610d491cf8e6cc5772f7eabc9e68f
CETECOM <cetecom_conce@duoc.cl>
2024-09-02 15:40:17 -0400

    header listo para prop

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EncabezadoComponent } from './encabezado/encabezado.component';
+import { IonicModule } from '@ionic/angular';
 
 
 
 @NgModule({
   declarations: [EncabezadoComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    IonicModule
   ],
   exports:[EncabezadoComponent]
 })
