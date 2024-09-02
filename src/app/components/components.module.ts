01ab50c33e7ad890543de7bd6ad04fa35af353c5
CETECOM <cetecom_conce@duoc.cl>
2024-09-02 15:36:14 -0400

    Encabezado works

---
+++ src/app/components/components.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { EncabezadoComponent } from './encabezado/encabezado.component';
+
+
+
+@NgModule({
+  declarations: [EncabezadoComponent],
+  imports: [
+    CommonModule
+  ],
+  exports:[EncabezadoComponent]
+})
+export class ComponentsModule { }
