c56fdddbf232356b192a54882363220011672c70
CETECOM <cetecom_conce@duoc.cl>
2024-09-04 12:55:09 -0400

    Prop ahora si

--- src/app/components/encabezado/encabezado.component.ts
+++ src/app/components/encabezado/encabezado.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-encabezado',
@@ -8,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class EncabezadoComponent  implements OnInit {
 
   constructor() { }
+
+  @Input() titulo:string=''
 
   ngOnInit() {}
 
