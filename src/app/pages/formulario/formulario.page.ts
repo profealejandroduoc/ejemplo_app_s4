2b2136d028685cb645e51662f5d8d9644c507a5c
El profe <al.sepulvedam@profesor.duoc.cl>
2024-09-11 12:52:24 -0300

    Formulario

--- src/app/pages/formulario/formulario.page.ts
+++ src/app/pages/formulario/formulario.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Usuario } from 'src/app/interfaces/usuario';
 
 @Component({
   selector: 'app-formulario',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormularioPage implements OnInit {
 
-  constructor() { }
+  mensaje:string=''
+  usr:Usuario={
+    username:'',
+    password:''
+  }
+
+  constructor(private router:Router) { }
 
   ngOnInit() {
+  }
+
+  onSubmit(){
+    console.log(this.usr);
+    if(this.usr.username=="wacoldo" && this.usr.password=="123"){
+      console.log("Acceso ok");
+        this.router.navigate(['/home'])
+    }
+    else{
+      this.mensaje='Acceso Denegado';
+    }
   }
 
 }
